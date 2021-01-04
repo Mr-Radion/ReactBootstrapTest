20715bb556896858cc5cafe37ee4bc092b6344f9
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-04 20:28:08 +0300

    registration form and cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import { Slider } from "./components";
+import React from 'react';
+import { Slider } from './components';
+import { Button, Container, Row, Col, Card } from 'react-bootstrap';
+import sunset from './sunset.jpg';
+
+const cards = [1, 2, 3, 4];
 
 function Home() {
   return (
-    <Slider />
-  )
+    <>
+      <Slider />
+      <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+        <Row>
+          {cards &&
+            cards.map((card) => (
+              <Col key={card} style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={sunset}></Card.Img>
+                  <Card.Body>
+                    <Card.Title>RaDrIgIs blog</Card.Title>
+                    <Card.Text>Amet id cillum reprehenderit ullamco.</Card.Text>
+                    <Button variant="primary">Learn more</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { Navbar, Nav, Button, Container, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -15,11 +15,16 @@ const Styles = styled.div`
 `;
 
 function NaviBar() {
+  const [show, setOpen] = React.useState<boolean>(false);
+
+  const handleShow = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Styles>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
+          <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
             <Navbar.Brand>RaDrIgIs blog</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -36,13 +41,38 @@ function NaviBar() {
                 </Nav.Link>
               </Nav>
               <Nav>
-                <Button variant="primary">Log in</Button>
-                <Button variant="primary">Sign out</Button>
+                <Button variant="primary" className="mr-2" onClick={handleShow}>
+                  Log in
+                </Button>
+                <Button variant="primary" onClick={handleShow}>
+                  Sign out
+                </Button>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </Styles>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email Adress</Form.Label>
+              <Form.Control type="email" placeholder="Enter Email" />
+              <Form.Text className="text-muted">We`ll never share your email with</Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
